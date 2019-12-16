2142c562061756661081e86440a5b26aa4040a8b
Lucas Felicíssimo <lucas.cesar.ti@gmail.com>
2019-12-16 16:43:19 -0300

    Changed app structure order

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
@@ -15,14 +26,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
